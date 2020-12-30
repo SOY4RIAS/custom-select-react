06051d3407948576518adac476c0b5d8407841fb
Santiago Arias <32374120+SOY4RIAS@users.noreply.github.com>
2020-12-30 01:21:36 +0000

    Implement sass

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
 import React, { useState } from "react";
+
+import styles from './App.module.scss'
+
 import styled from "styled-components";
-
-const Main = styled("div")`
-  font-family: sans-serif;
-  background: #f0f0f0;
-  height: 100vh;
-`;
-
-const DropDownContainer = styled("div")`
-  width: 10.5em;
-  margin: 0 auto;
-`;
-
-const DropDownHeader = styled("div")`
-  margin-bottom: 0.8em;
-  padding: 0.4em 2em 0.4em 1em;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
-  font-weight: 500;
-  font-size: 1.3rem;
-  color: #3faffa;
-`;
-
-const DropDownListContainer = styled("div")`
-  position: absolute;
-  z-index: 100;
-  width: 10.5em;
-`;
-
-const DropDownList = styled("ul")`
-  padding: 0;
-  margin: 0;
-  padding-left: 1em;
-  background: #ffffff;
-  border: 2px solid #e5e5e5;
-  box-sizing: border-box;
-  color: #3faffa;
-  font-size: 1.3rem;
-  font-weight: 500;
-  &:first-child {
-    padding-top: 0.8em;
-  }
-`;
-
-const ListItem = styled("li")`
-  list-style: none;
-  margin-bottom: 0.8em;
-  &:hover {
-    color: #fd9e46;
-  }
-`;
 
 const options = ["Mangoes", "Apples", "Oranges"];
 
@@ -65,24 +19,21 @@ export default function App() {
   };
 
   return (
-    <Main>
-      <h1>Custom Select/dropdown</h1>
-      <DropDownContainer>
-        <DropDownHeader onClick={toggling}>
-          {selectedOption || "Mangoes"}
-        </DropDownHeader>
-        {isOpen && (
-          <DropDownListContainer>
-            <DropDownList>
-              {options.map(option => (
-                <ListItem onClick={onOptionClicked(option)} key={Math.random()}>
-                  {option}
-                </ListItem>
-              ))}
-            </DropDownList>
-          </DropDownListContainer>
-        )}
-      </DropDownContainer>
-    </Main>
+    <div className={styles.dropdownContainer}>
+      <div className={styles.dropdownHeader} onClick={toggling}>
+        {selectedOption || "Mangoes"}
+      </div>
+      {isOpen && (
+        <div className={styles.dropdownListContainer}>
+          <div className={styles.dropdownList}>
+            {options.map(option => (
+              <div className={styles.listItem} onClick={onOptionClicked(option)} key={Math.random()}>
+                {option}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
